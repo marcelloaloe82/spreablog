5f5824ee1ecaeb6c9d7cb267f65f60adb6f089c2
marcelloaloe82 <marcaloe@gmail.com>
2019-01-30 17:42:55 +0100

    work

--- application/controllers/api/Comments.php
+++ application/controllers/api/Comments.php
@@ -24,29 +24,34 @@ class Comments extends REST_Controller {
     //lista dei commenti
     function index_get(){
 
-    	if(empty($this->session->user)){
-    		$this->set_response(NULL, REST_Controller::HTTP_FORBIDDEN);
-    	}
-
-    	$user_id = $this->session->user['id'];
-
-    	$comments = $this->comment->all($user_id);
-
-    	foreach ($comments as $key => $row) {
+    	if(!empty($this->session->user)){
     		
-    		$comments[$key]['rispondi'] = sprintf("<a href=\"%sindex.php/api/comments/reply/%s\"><span class=\"glyphicon glyphicon-send\"></span></a>", base_url(), $comments[$key]['id']);
-			
-			$comments[$key]['cancella'] = sprintf("<a href=\"%sindex.php/api/comments/delete/%s\"><span class=\"glyphicon glyphicon-remove	\"></span></a>", base_url(), $comments[$key]['id']);
-			
-			$comments[$key]['view_comment'] = "<a href='#'><span class=\"glyphicon glyphicon-eye-open\"></span></a>";
 
-            unset($comments[$key]['id']);
-    	}
+        	$user_id = $this->session->user['id'];
 
-        if(count($comments) === 0)
-            $comments = [];
+        	$comments = $this->comment->all($user_id);
 
-    	$this->set_response($comments, REST_Controller::HTTP_OK);
+        	foreach ($comments as $key => $row) {
+        		
+        		$comments[$key]['rispondi'] = sprintf("<a href=\"%sindex.php/api/comments/reply/%s\"><span class=\"glyphicon glyphicon-send\"></span></a>", base_url(), $comments[$key]['id']);
+    			
+    			$comments[$key]['cancella'] = sprintf("<a href=\"%sindex.php/api/comments/delete/%s\"><span class=\"glyphicon glyphicon-remove	\"></span></a>", base_url(), $comments[$key]['id']);
+    			
+    			$comments[$key]['view_comment'] = "<a href='#'><span class=\"glyphicon glyphicon-eye-open\"></span></a>";
+
+                unset($comments[$key]['id']);
+        	}
+
+            if(count($comments) === 0)
+                $comments = [];
+
+        	$this->set_response($comments, REST_Controller::HTTP_OK);
+
+
+        } else{
+
+            $this->set_response(NULL, REST_Controller::HTTP_FORBIDDEN);
+        }
     }
 
     
@@ -60,68 +65,71 @@ class Comments extends REST_Controller {
         $captcha_key = 'g-recaptcha-response';
 
 
-        if(!$this->input->post($captcha_key)) {
-            $this->set_response(NULL, REST_Controller::HTTP_FORBIDDEN);
-            return;
-        }
-
-        $url = 'https://www.google.com/recaptcha/api/siteverify';
-        $secret = "6LffHIwUAAAAAA96PfNPKALU9ZXDqcsjsEbWQecK";
-        $data = array('response' => $this->input->post($captcha_key) , 'secret' => $secret);
-                
-        $options = array(
-            'http' => array(
-                'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
-                'method'  => 'POST',
-                'content' => http_build_query($data)
-            )
-        );
-        
-        $context  = stream_context_create($options);
-        $result = file_get_contents($url, false, $context);
-        
-        if ($result === FALSE)  {
+        if($this->input->post($captcha_key)) {
             
-            $this->set_response(['message' => $message_captcha], REST_Controller::HTTP_FORBIDDEN);
+
+            $url = 'https://www.google.com/recaptcha/api/siteverify';
+            $secret = "6LffHIwUAAAAAA96PfNPKALU9ZXDqcsjsEbWQecK";
+            $data = array('response' => $this->input->post($captcha_key) , 'secret' => $secret);
+                    
+            $options = array(
+                'http' => array(
+                    'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
+                    'method'  => 'POST',
+                    'content' => http_build_query($data)
+                )
+            );
+            
+            $context  = stream_context_create($options);
+            $result = file_get_contents($url, false, $context);
+            
+            if ($result === FALSE)  {
+                
+                $this->set_response(['message' => $message_captcha], REST_Controller::HTTP_FORBIDDEN);
+            }
+
+            unset($comment_data[$captcha_key]);   
+            $comment_data['ip_address'] = $this->input->ip_address();     
+
+            
+            try{
+                $this->comment->save($comment_data);
+                $this->set_response(['message'=>$message_ok], REST_Controller::HTTP_OK);
+
+            }catch(Exception $e){
+
+                $error_message = $e->getMessage();
+                $this->set_response(['message'=>$error_message], REST_Controller::HTTP_INTERNAL_SERVER_ERROR);
+            }
+
+
+        } else{
+
+            $this->set_response(NULL, REST_Controller::HTTP_FORBIDDEN);
         }
-
-        unset($comment_data[$captcha_key]);   
-        $comment_data['ip_address'] = $this->input->ip_address();     
-
-        
-        try{
-            $this->comment->save($comment_data);
-            $this->set_response(['message'=>$message_ok], REST_Controller::HTTP_OK);
-
-        }catch(Exception $e){
-
-            $error_message = $e->getMessage();
-            $this->set_response(['message'=>$error_message], REST_Controller::HTTP_INTERNAL_SERVER_ERROR);
-        }
-
-
-    }
 
 
     public function reply_post($comment_id){
 
         $message_ok = 'Risposta inviata';
 
-    	if(empty($this->session->user)){
-    		$this->set_response(NULL, REST_Controller::HTTP_FORBIDDEN);
-    	}
+    	if(!empty($this->session->user)){
+    		
+            try{
 
-        try{
+        	   $this->comment->reply($comment_id, $this->input->post());
+               $this->set_response(['message'=>$message_ok], REST_Controller::HTTP_OK);
+            
+            }catch(Exception $e){
 
-    	   $this->comment->reply($comment_id, $this->input->post());
-           $this->set_response(['message'=>$message_ok], REST_Controller::HTTP_OK);
+                $error_message = $e->getMessage();
+                $this->set_response(['message'=>$error_message], REST_Controller::HTTP_INTERNAL_SERVER_ERROR);
+            }
         
-        }catch(Exception $e){
+        } else{
 
-            $error_message = $e->getMessage();
-            $this->set_response(['message'=>$error_message], REST_Controller::HTTP_INTERNAL_SERVER_ERROR);
+            $this->set_response(NULL, REST_Controller::HTTP_FORBIDDEN);
         }
-
 
 
     }
@@ -133,19 +141,24 @@ class Comments extends REST_Controller {
         $message_ok = 'Commento cancellato';
         $error_message = '';
 
-    	if(empty($this->session->user)){
-    		$this->set_response(NULL, REST_Controller::HTTP_FORBIDDEN);
-    	}
+    	if(!empty($this->session->user)){
+            
+            try{
+                $this->comment->delete($comment_id);
+                $this->set_response(['message'=>$message_ok], REST_Controller::HTTP_OK);
+            
+            } catch(Exception $e){
 
-        try{
-            $this->comment->delete($comment_id);
-            $this->set_response(['message'=>$message_ok], REST_Controller::HTTP_OK);
-        
-        } catch(Exception $e){
+                $error_message = $e->getMessage();
+                $this->set_response(['message'=>$error_message], REST_Controller::HTTP_INTERNAL_SERVER_ERROR);
+            }
 
-            $error_message = $e->getMessage();
-            $this->set_response(['message'=>$error_message], REST_Controller::HTTP_INTERNAL_SERVER_ERROR);
+        } else{
+
+            $this->set_response(NULL, REST_Controller::HTTP_FORBIDDEN);
         }
+
+
 
 
     }
