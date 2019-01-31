a1a89a39bb72c83c26d675a0a39db21fc95f56d8
marcelloaloe82 <marcaloe@gmail.com>
2019-01-31 12:30:42 +0100

    work

--- application/views/single_news.php
+++ application/views/single_news.php
@@ -93,7 +93,7 @@
         Vuoi davvero cancellare la news?
       </div>
       <div class="modal-footer">
-         <button id="butt-ok" type="button" class="btn btn-success" data-dismiss="modal">OK</button>
+         <button id="butt-ok" type="button" class="btn btn-danger" data-dismiss="modal">OK</button>
          <button type="button" class="btn btn-default" data-dismiss="modal">Annulla</button>
       </div>
     </div>
@@ -114,8 +114,8 @@
         Vuoi davvero cancellare la news?
       </div>
       <div class="modal-footer">
-         <button id="butt-ok" type="button" class="btn btn-danger" data-dismiss="modal">OK</button>
-         <button type="button" class="btn btn-default" data-dismiss="modal">Annulla</button>
+         <button id="butt-ok" type="button" class="btn btn-default" data-dismiss="modal">Chiudi</button>
+         
       </div>
     </div>
 
