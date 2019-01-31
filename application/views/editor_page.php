616f61c51de48616bfeadfdbb2bb23925561355b
marcelloaloe82 <marcaloe@gmail.com>
2019-01-31 17:59:54 +0100

    work

--- application/views/editor_page.php
+++ application/views/editor_page.php
@@ -73,14 +73,14 @@
 
   </div>
 </div>
-<div id="message-modal" class="modal fade" role="dialog">
+<div id="message-dialog" class="modal fade" role="dialog">
   <div class="modal-dialog">
 
     <!-- Modal content-->
     <div class="modal-content">
       <div class="modal-header">
         <button type="button" class="close" data-dismiss="modal">&times;</button>
-        <h4 class="modal-title">Operazione in corso...</h4>
+        <h4 class="modal-title">Risultato operazione</h4>
       </div>
       <div class="modal-body">
         
