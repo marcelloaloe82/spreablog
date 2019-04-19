755255e9a28ac77eb0464535a210767d828e3631
marcelloaloe82 <marcaloe@gmail.com>
2019-04-19 11:57:31 +0200

    work

--- application/views/parti/tab_editor.php
+++ application/views/parti/tab_editor.php
@@ -1,4 +1,4 @@
-<div id="modera" class="tab-pane fade <?php $ruolo_utente == 'editor' ? echo 'active' : echo ''; ?>"  >
+<div id="modera" class="tab-pane fade <?php echo ($ruolo_utente == 'editor' ?  'active' : ''); ?>"  >
       
       <table id="commenti" class="cell-border compact stripe" style="width: 100%;">
           <thead>
